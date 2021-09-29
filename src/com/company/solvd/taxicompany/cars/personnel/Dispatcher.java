67f46d2f78af8382b9fc4aba133baff9028c0e92
Kristina Petrushko <kristinapetrushko@MacBook-Air-Kristina.local>
2021-09-29 23:14:17 +0300

    update src/com/company/solvd/taxicompany/cars/personnel/Dispatcher.java

--- src/com/company/solvd/taxicompany/cars/personnel/Dispatcher.java
+++ src/com/company/solvd/taxicompany/cars/personnel/Dispatcher.java
@@ -1,2 +1,18 @@
-package com.company.solvd.taxicompany.cars.personnel;public class Dispatcher {
+package com.company.solvd.taxicompany.cars.personnel;
+
+public class Dispatcher extends Person {
+    private String workTime;
+
+    public Dispatcher(String FIO, int age, String education) {
+        super(FIO, age, education);
+        this.workTime = workTime;
+    }
+
+    public String getWorkTime () {
+        return workTime;
+    }
+
+    public void setWorkTime (String workTime) {
+        this.workTime = workTime;
+    }
 }
