59215d2460084928bd41e72e0c09c0619b317327
Kristina Petrushko <kristinapetrushko@MacBook-Air-Kristina.local>
2021-09-29 23:16:37 +0300

    update src/com/company/solvd/taxicompany/cars/LORRY.java

--- src/com/company/solvd/taxicompany/cars/LORRY.java
+++ src/com/company/solvd/taxicompany/cars/LORRY.java
@@ -1,2 +1,27 @@
-package com.company.solvd.taxicompany.cars;public class LORRY {
+package com.company.solvd.taxicompany.cars;
+
+public class LORRY extends Car {
+    private int weight;
+    private int load;
+
+    public LORRY(String model, int year, int passengers) {
+        super(model, year, passengers);
+        this.weight = weight;
+        this.load = load;
+    }
+
+    public int getWeight (int weight) {
+        return weight;
+    }
+
+    public void setWeight (int weight) {
+        this.weight = weight;
+    }
+
+    public int getLoad (int load) {
+        return load;
+    }
+    public void setLoad (int load) {
+        this.load = load;
+    }
 }
